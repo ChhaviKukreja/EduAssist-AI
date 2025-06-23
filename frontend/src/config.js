3b9dd3ecc9cff72e125139032ecc8e626bff7df3
ChhaviKukreja <chhavikukreja19@gmail.com>
2025-06-23 18:00:03 +0530

    extra slash 3

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 const config = {
     // API_BASE_URL: "http://localhost:5000"
-    API_BASE_URL: "https://eduassistbackend-chhavikukrejas-projects.vercel.app/"
+    API_BASE_URL: "https://eduassistbackend-chhavikukrejas-projects.vercel.app"
 };
 
 export default config;
